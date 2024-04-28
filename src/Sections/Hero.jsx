1257b7ddf3364d526c5676d68b8f0a5ea623703e
cgrade <gradeisageek@gmail.com>
2024-04-28 21:23:32 +0100

    shoe card added

--- src/Sections/Hero.jsx
+++ src/Sections/Hero.jsx
@@ -1,6 +1,7 @@
 import { Button } from "../Components/Button"
 import { arrowRight } from "../assets/icons"
-import { statistics } from "../constants"
+import { bigShoe1 } from "../assets/images"
+import { shoes, statistics } from "../constants"
 
 export const Hero = () => {
   return (
@@ -21,19 +22,39 @@ export const Hero = () => {
         </h1>
         <p 
           className="font-montserrat text-slate-gray text-lg leading-8 mt-6 mb-14 gap-16"
-        >Discover Stylish Nker arrivasl, quality comfort and innovation for you acitve life</p>
+        >Discover Stylish Nike arrivasl, quality comfort and innovation for you acitve life</p>
         <Button label="Shop n ow" iconURL={arrowRight}/>
         <div className="flex justify-starts items-start flex-wrap w-full mt-20 gap-16  ">
         {statistics.map((stat, index) => (
           <div key={stat.label}>
-            <p>{stat.value} </p>
-            <p>{stat.label}</p>
+            <p
+            className="text-4xl font-palanquin font-bold"
+            >{stat.value} </p>
+            <p 
+            className="leading-7 font-montserrat text-slate-gray"
+            >{stat.label}</p>
           </div>
         ))}
         </div>
       </div>
  
-
+      <div className="relative flex-1 flex justify-center items-center 
+      xl:min-h-screen max-xl: py-40 bg-primary bg hero bg-cover bg-center ">
+        <img 
+         src={bigShoe1} 
+         alt="Shoe Collection"
+         width={610}
+         height={500} 
+         className="object-contain relative z-10"/>
+         <div>
+          {shoes.map((shoe, index) =>(
+            <div key={shoe}> 
+            <ShoeCard />
+            </div>
+          ))}
+         </div>
+         
+      </div>
     </section>
   )
 }
