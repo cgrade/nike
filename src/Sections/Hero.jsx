e00d23e7a380f34fddbb286cbdb7f85ee61c66f1
cgrade <gradeisageek@gmail.com>
2024-04-28 13:15:34 +0100

    initial project commit

---
+++ src/Sections/Hero.jsx
@@ -0,0 +1,22 @@
+
+
+export const Hero = () => {
+  return (
+    <section id="home"
+    className=" w-full flex xl:flex-row flex-col justify-center
+    min-h-screen gap-10 max-container">
+      <div className="relate4ive xl:w-2/5 flex flex-col justify-center items-start w-full max-xl:padding-x pt-28">
+        <p>Our Summer collection</p>
+        <h1>
+          <span>The New Arrival</span>
+          <br />
+          <span>Nike</span> Shoes
+        </h1>
+        <p>Discover Stylish Nker arrivasl, quality comfort and innovation for you acitve life</p>
+        
+      </div>
+
+
+    </section>
+  )
+}
---
+++ src/Sections/index.js
@@ -0,0 +1,13 @@
+import {Hero} from './Hero';
+import { PopularProducts } from './PopularProducts';
+import { Footer } from './Footer';
+import { Services } from './Services';
+import { SuperQuality } from './SuperQuality';
+import { SpecialOffer } from './SpecialOffer';
+import { Subscribe } from './Subscribe';
+import { CustomerReviews } from './CustomerReviews';
+
+export { 
+    Hero, PopularProducts, Footer, Services, SuperQuality, SpecialOffer,
+    Subscribe, CustomerReviews
+ };
