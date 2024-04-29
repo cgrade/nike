52438779ecf7b8c05e4ff461824e4c86e1bf1b02
cgrade <gradeisageek@gmail.com>
2024-04-29 12:16:57 +0100

    code update

--- src/constants/index.js
+++ src/constants/index.js
@@ -33,22 +33,22 @@ export const products = [
     {
         imgURL: shoe4,
         name: "Nike Air Jordan-01",
-        price: "$200.20",
+        price: "ETH 0.016",
     },
     {
         imgURL: shoe5,
         name: "Nike Air Jordan-10",
-        price: "$210.20",
+        price: "ETH 0.020",
     },
     {
         imgURL: shoe6,
         name: "Nike Air Jordan-100",
-        price: "$220.20",
+        price: "ETH 0.011",
     },
     {
         imgURL: shoe7,
         name: "Nike Air Jordan-001",
-        price: "$230.20",
+        price: "ETH 0.022",
     },
 ];
 
